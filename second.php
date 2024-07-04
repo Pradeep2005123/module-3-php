e3ad5d6c20cd798715b010eeb7bd0cb8eac11f28
Pradeep2005123 <ps0587713@gmail.com>
2024-07-04 21:50:09 +0530

    save it

---
+++ second.php
@@ -0,0 +1,13 @@
+<?php
+$pi=3.14;
+$r=5;
+if ($r > 5)
+{
+$volume_sphere=(4/3)*$pi * $r *$r *$r;
+}
+else 
+{
+    $volume_sphere=1500;
+}
+echo "The volume of the sphere is : ", $volume_sphere;
+?>
